5160286066302beed3c614177ef0584bad5ea6f3
Nayo Akinyele <akinyelenayo@gmail.com>
2021-05-04 09:24:38 +0100

    Add brew and git config

---
+++ .brew.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+
+# Install tools
+brew install git
+brew install grep
+brew install mob
+brew install awscli
+brew install terragrunt
+brew install tfenv
+brew install watch
+brew install zsh
+brew install jq
+brew install go
+brew install sops
+brew install tree
+
+# Remove outdated versions from the cellar.
+brew cleanup
